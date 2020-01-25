f3afda059ccb07ffccce51e14075a9043b496d92
David Omstead <49734565+DOmstead@users.noreply.github.com>
2020-01-24 21:52:27 -0700

    New project added and HTML updated
    An additional project was added the HTML was updated to comply with W3C standards. Additional code changes were made to support these updates. Additional nav bar functionality was also added.

--- index.js
+++ index.js
@@ -9,29 +9,43 @@ $(document).ready(function(){
   /* Event Listeners Below */
   hamburger();
   cross();
-  checkHamburger();
+  resizeCheck();
   greeting();
   background();
+  linkSelected();
 });
 
 //This function deals with the hamburger button on the Navbar.
 function hamburger(){
   $('.hamburger').click( function(){
-    $('.nav a').css('display', 'block');
+    $('.nav li').css('display', 'block');
     $('.nav-home').css('max-width','85%');
     $('.hamburger').hide();
     $('.cross').show();
   });
 }
 
-//This function deals with the cross button on the Navbar.
-function cross(){
-  $('.cross').click( function(){
+function closeLinks(){
+//This block of code runs when the screen is smaller than 700px
+  if( $('.skillSymbolsAndList').css('display') !== 'flex'){
     $('.nav-link').css('display', 'none');
     $('.nav-home').css('max-width', '65px');
     $('.cross').hide();
     $('.hamburger').show();
+    console.log('Close function ran');
+  }}
+
+//This function deals with the cross button on the Navbar.
+function cross(){
+  $('.cross').click( function(){
+    closeLinks();
   });
+}
+
+function linkSelected(){
+  $('.nav-link,.nav-home').click( function(){
+    closeLinks();
+  }); 
 }
 
 //This function prints a greeting to the console for anyone interested. If you're 
@@ -40,15 +54,14 @@ function greeting(){
   console.log('Hello there! Thank you for taking the time to view my website. Since you are looking at the console you are likely a fellow developer, hiring manager, or recruiter. This site was created using HTML5, CSS3, JavaScript, and jQuery. Webpack was also used during the development phase. Should you want to see the code it is all available on my Github page. A link to my account is provided at the bottom of the page. Please feel free to reach out and let me know what you think! I’m always open to feedback, conversations, or new opportunities. Thank you again and have a great day! David Omstead');
 }
 
-//This function chnages the background image for section headers.
+//This function changes the background image for section headers.
 function background(){
   $('.skills h2, .projects h2, .about-me h2, .contact-me h2').css('background-image','url("ImageFiles/TreeBackground1.png")');
 }
 
 //This function runs when the screen is resized. It ensures that the nav bar
 //components display correctly when the screen is resized. 
-
-function checkHamburger(){
+function resizeCheck(){
   $(window).on('resize',function(){
 
     //This block of code runs when the screen is larger than 700px
@@ -67,3 +80,4 @@ function checkHamburger(){
     } 
   });
 }
+
